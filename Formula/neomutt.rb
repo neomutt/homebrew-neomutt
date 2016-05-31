023da876a8b1c53b6a97c1e511870b63062b8cc3
Alex Pearce <alex@alexpearce.me>
2016-05-31 07:13:21 +0200

    Use non-tagged commit to test neomutt/neomutt#37.

--- Formula/neomutt.rb
+++ Formula/neomutt.rb
@@ -1,7 +1,7 @@
 class Neomutt < Formula
   desc "Bringing together all the Mutt Code"
   homepage "http://www.neomutt.org/"
-  url "https://github.com/neomutt/neomutt.git", :tag => "neomutt-20160530", :revision => "c68719343c86cf2ddefa54d96328afb3ac371e97"
+  url "https://github.com/neomutt/neomutt.git", :revision => "7742cc7e48b8fa967c75ecc9f5fc6609a4af1bc5"
   head "https://github.com/neomutt/neomutt.git", :branch => "neomutt"
 
   conflicts_with "tin",
