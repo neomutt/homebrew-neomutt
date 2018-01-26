6a6c72f6f12d7ae32ade8d2f9511f1ca8f2e805a
Jakub Jindra <jakub.jindra@socialbakers.com>
2018-01-26 16:15:13 +0100

    Tidy up Formula
    * Removed up old buildd eps
    * Default header cache backend to tokyo-cabinet
    * Removed conflict with tin Formula
    * Changed descripion
    * Make lmdb and tokyo-cabinet dependencies mutualy exclusive

--- Formula/neomutt.rb
+++ Formula/neomutt.rb
@@ -1,32 +1,28 @@
 class Neomutt < Formula
-  desc "Bringing together all the Mutt Code"
+  desc "Teaching an Old Dog New Tricks"
   homepage "https://www.neomutt.org/"
   url "https://github.com/neomutt/neomutt.git", :tag => "neomutt-20171215", :revision => "ae61285170533b4be544e738cdd2eedefd1856ff"
   head "https://github.com/neomutt/neomutt.git", :branch => "master"
 
+  option "with-lmdb", "Build with lmdb support"
   option "with-lua", "Build with lua scripting support enabled"
   option "with-s-lang", "Build against slang instead of ncurses"
 
   # Neomutt-specific patches
   option "with-notmuch-patch", "Apply notmuch patch"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "gettext" => :build
   depends_on "docbook" => :build
   depends_on "docbook-xsl" => :build
 
   depends_on "openssl"
-  depends_on "tokyo-cabinet" => :optional
+  depends_on "tokyo-cabinet" => :recommended unless build.with?("lmdb")
   depends_on "lmdb" => :optional
   depends_on "gpgme" => :optional
   depends_on "libidn" => :optional
   depends_on "lua" => :optional
   depends_on "s-lang" => :optional
   depends_on "notmuch" if build.with? "notmuch-patch"
-
-  conflicts_with "tin",
-    :because => "both install mmdf.5 and mbox.5 man pages"
 
   def install
     # Find our docbook catalog
@@ -47,9 +43,7 @@ class Neomutt < Formula
 
     if build.with? "lmdb"
       args << "--lmdb"
-    end
-
-    if build.with? "tokyo-cabinet"
+    else
       args << "--tokyocabinet"
     end
 
